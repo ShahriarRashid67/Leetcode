fb88d0ab02c2c3b5e5799ad1e40bb7acf0858a7d
ShahriarRashid67 <102477800+ShahriarRashid67@users.noreply.github.com>
2022-11-13 20:33:30 +0600

    Time: 7 ms (10.42%), Space: 6.5 MB (53.42%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,6 +1,7 @@
-class Solution {
+class Solution
+{
 public:
-     unordered_map<char, string> mp {
+    unordered_map<char, string> mp{
         {'2', "abc"},
         {'3', "def"},
         {'4', "ghi"},
@@ -8,24 +9,29 @@ public:
         {'6', "mno"},
         {'7', "pqrs"},
         {'8', "tuv"},
-        {'9', "wxyz"}
-    };
-    void gen(int pos,string &digits,vector<string>&ans,string tmp=""){
-        if(digits.size()==tmp.size()){
+        {'9', "wxyz"}};
+    
+    void gen(int pos, string &digits, vector<string> &ans, string tmp = "")
+    {
+        if (digits.size() == tmp.size())
+        {
             ans.push_back(tmp);
             return;
         }
-       
-        for(auto c: mp[digits[pos]]){
-            tmp+=c;
-            gen(pos+1,digits,ans,tmp);
+
+        for (auto c : mp[digits[pos]])
+        {
+            tmp += c;
+            gen(pos + 1, digits, ans, tmp);
             tmp.pop_back();
         }
     }
-    vector<string> letterCombinations(string digits) {
-        vector<string>ans;
-        if(digits.size()!=0){
-        gen(0,digits,ans);
+    vector<string> letterCombinations(string digits)
+    {
+        vector<string> ans;
+        if (digits.size() != 0)
+        {
+            gen(0, digits, ans);
         }
         return ans;
     }
