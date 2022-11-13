24cdeb854c3e6d025a417265d279be15a2773a3c
ShahriarRashid67 <102477800+ShahriarRashid67@users.noreply.github.com>
2022-11-13 19:57:48 +0600

    Time: 0 ms (100.00%), Space: 8.1 MB (44.99%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,23 +1,32 @@
-class Solution {
+class Solution
+{
 public:
-    string reverseWords(string s) {
-        string tmp="";
-        vector<string>vc;
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=' ') tmp+=s[i];
-            else {
-               if(tmp!="") vc.push_back(tmp);
-                tmp="";
+    string reverseWords(string s)
+    {
+        string tmp = "";
+        vector<string> vc;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] != ' ')
+                tmp += s[i];
+            else
+            {
+                if (tmp != "")
+                    vc.push_back(tmp);
+                tmp = "";
             }
         }
-        
-       if(tmp!="") vc.push_back(tmp);
-        string ans="";
-        for(int i=vc.size()-1;i>=0;i--){
-            ans+=vc[i];
-            
-            if(i!=0){
-            ans+=' ';
+
+        if (tmp != "")
+            vc.push_back(tmp);
+        string ans = "";
+        for (int i = vc.size() - 1; i >= 0; i--)
+        {
+            ans += vc[i];
+
+            if (i != 0)
+            {
+                ans += ' ';
             }
         }
         return ans;
