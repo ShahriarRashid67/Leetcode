00ad6ab229e38a5216e477b2894a7e2aa826cf3d
ShahriarRashid67 <102477800+ShahriarRashid67@users.noreply.github.com>
2022-11-16 19:19:20 +0600

    Time: 3 ms (82.65%), Space: 6.3 MB (56.70%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -5,7 +5,7 @@ public:
         reverse(a.begin(),a.end());
         reverse(b.begin(),b.end());
         int i=0;
-        char c='0';
+        int c=0;
        
         while(i<a.size() || i<b.size()){
             int x=0;
@@ -17,24 +17,12 @@ public:
                 y=b[i]-'0';
             }
           
-            int sum=x+y+c-'0';
-            if(sum==3){
-                c='1';
-                s+='1';
-            }else if(sum==2){
-                c='1';
-                s+='0';
-            }else if(sum==1){
-                c='0';
-                s+='1';
-            }else{
-                c='0';
-                s+='0';
-            }
-           
+            int sum=x+y+c;
+            s+=(sum%2+'0');
+            c=(sum/2);
             i++;
         }
-        if(c=='1'){
+        if(c==1){
         s+='1';
         }
         reverse(s.begin(),s.end());
