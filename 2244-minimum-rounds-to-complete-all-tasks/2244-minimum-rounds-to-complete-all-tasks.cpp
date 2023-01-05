3d7fcc7371dcae50b181ee8bd2123ce9949baae4
ShahriarRashid67 <102477800+ShahriarRashid67@users.noreply.github.com>
2023-01-05 18:40:17 +0600

    Time: 225 ms (89.93%), Space: 103.6 MB (71.69%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
-       int ans=0;
-        unordered_map<int,int>freq;
-        for(int i=0;i<tasks.size();i++){
-            freq[tasks[i]]++;
+     unordered_map<int,int>mp;
+        for(auto x:tasks) mp[x]++;
+        int ans=0;
+        for(auto x: mp){
+            int t=x.second;
+            if(t==1) return -1;
+            if(t%3==0){
+                ans+=(t/3);
+            }else{
+                ans+=((t/3)+1);
+            
+            }
         }
-        for(auto it:freq){
-            int k=it.second;
-            if(k==1)return -1;
-            if(k%3==0)
-            ans+=k/3;
-            else 
-            ans+=(k/3)+1;
-        }
+        
      return ans;  
     }
 };
