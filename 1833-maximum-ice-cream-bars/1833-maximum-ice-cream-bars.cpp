521b3ec926ebda90aa83e6e54ad6b5e1aa795354
ShahriarRashid67 <102477800+ShahriarRashid67@users.noreply.github.com>
2023-01-07 00:01:15 +0600

    Time: 443 ms (27.64%), Space: 76.5 MB (32.82%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        int n = costs.size();
-        int m = *max_element(costs.begin(), costs.end());
-        int icecreams = 0;
-
-        vector<int> costsFrequency(m + 1);
-        for (auto& cost : costs) {
-            costsFrequency[cost]++;
+        sort(costs.begin(),costs.end());
+        int ans=0;
+          for(int i=0;i<costs.size();i++){
+            if(costs[i]>coins)return ans;
+            coins -= costs[i];
+            ans++;
         }
-
-        for (int cost = 1; cost <= m; ++cost) {
-            // No ice cream is present costing 'cost'.
-            if (costsFrequency[cost] == 0) {
-                continue;
-            }
-            // We don't have enough 'coins' to even pick one ice cream.
-            if (coins < cost) {
-                break;
-            }
-            
-            // Count how many icecreams of 'cost' we can pick with our 'coins'.
-            // Either we can pick all ice creams of 'cost' or we will be limited by remaining 'coins'.
-            int count = min(costsFrequency[cost], coins / cost);
-            // We reduce price of picked ice creams from our coins.
-            coins -= cost * count;
-            icecreams += count;
-        }
-
-        return icecreams;
+        return ans; 
     }
 };
