4dc30ff0fc2eb1d16a245e124964a424335d1c53
ShahriarRashid67 <102477800+ShahriarRashid67@users.noreply.github.com>
2023-01-26 00:59:44 +0600

    Time: 6 ms (15.01%), Space: 7.4 MB (56.59%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void preprocess(string &s,int maxWidth,int last){
+        if(s.size()==maxWidth) return;
+        
+        int space=0;
+        for(auto c:s) {if(c==' ') space++;}
+        
+        int spneed=maxWidth-s.size();
+        if(space==0 || last==1){
+            s.insert(s.size(),spneed,' ');
+            return; 
+        }
+        int div=spneed/space;
+        int left=spneed%space;
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' ' && (div>0 || left>0)){
+                int sp = div + ((left--) > 0 ? 1:0);
+                s.insert(i,sp,' ');
+                i+=sp;
+            }
+        }
+       // cout<<s<<" :";
+    }
+ 
+    vector<string> fullJustify(vector<string>& s, int maxWidth) {
+        vector<string>vc;
+         string tmp = s[0];
+        for (int i = 1; i < s.size(); i++)
+        {
+            if (tmp.size() + s[i].size()+1 <=maxWidth)
+            {
+                tmp += (' '+s[i]);
+            }
+            else
+            {
+                vc.push_back(tmp);
+                tmp = s[i];
+            }
+        }
+        preprocess(tmp,maxWidth,1);
+        vc.push_back(tmp);
+        for(auto &str:vc){
+            preprocess(str,maxWidth,0);
+           
+        }
+        return vc;
+    }
+};
