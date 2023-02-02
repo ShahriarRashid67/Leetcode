9f126df6eec4cffba8eafa57531fc79e687d868d
ShahriarRashid67 <102477800+ShahriarRashid67@users.noreply.github.com>
2023-02-02 11:52:32 +0600

    Time: 10 ms (35.95%), Space: 9.6 MB (63.85%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     bool checker(string &s,string &t,vector<int>&val){
+       
         for(int i=0;i<min(s.size(),t.size());i++){
             if(val[s[i]]<val[t[i]]) return 1;
-            if(val[s[i]]==val[t[i]]) continue;
-             if(val[s[i]]>val[t[i]]) return 0;
+            if(val[s[i]]>val[t[i]]) return 0;
         }
+        
         return(s.size()<=t.size());
     }
-    ///abcd
-    //abba
+
     bool isAlienSorted(vector<string>& words, string order) {
         vector<int>val(130,0);
+        
         for(int i=0;i<order.size();i++){
             val[order[i]]=i+1;
         }
+        
         for(int i=0;i<words.size()-1;i++){
             if(!checker(words[i],words[i+1],val)){
                 return 0;
             }
         }
+        
         return 1;
     }
 };
