311bef73b37984f67aaab86c404445434425ffa6
ShahriarRashid67 <102477800+ShahriarRashid67@users.noreply.github.com>
2023-02-04 13:11:46 +0600

    Time: 8 ms (73.53%), Space: 7.4 MB (32.35%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    bool vcChecker(vector<int>&a,vector<int>&b){
-        for(int i=0;i<26;i++){
-            if(a[i]!=b[i]){
-            return 0;
-            }
+    bool equalChecker(vector<int>&ar,vector<int>&br){
+        for(int i=95;i<128;i++){
+            if(ar[i]!=br[i]) return 0;
         }
         return 1;
     }
-    
     bool checkInclusion(string s1, string s2) {
         if(s1.size()>s2.size()) return 0;
-      int n=s1.size();
-      vector<int>freq1(26,0),freq2(26,0);
-        for(int i=0;i<s1.size();i++)freq1[s1[i]-'a']++;
-        int l=0,r=n;
-        for(int i=0;i<n;i++){
-            freq2[s2[i]-'a']++;
+      vector<int>freq1(140,0),freq2(140,0);
+        for(auto c:s1) freq1[c]++;
+      
+        int l=0,r=0;
+        while(r<s1.size()){
+            freq2[s2[r]]++;
+            r++;
         }
         int ans=0;
-         ans|=vcChecker(freq1,freq2);
+        ans|=equalChecker(freq1,freq2);
         while(r<s2.size()){
-           
-            freq2[s2[r]-'a']++;
-            freq2[s2[l]-'a']--;
-             ans|=vcChecker(freq1,freq2);
-            r++;l++;
+            freq2[s2[l]]--;
+            freq2[s2[r]]++;
+            ans|=equalChecker(freq1,freq2);
+            l++;
+            r++;
         }
         return ans;
+        
     }
 };
