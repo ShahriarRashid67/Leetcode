0d24b15d6d93c8bfc16368da80589643dac84e8b
ShahriarRashid67 <102477800+ShahriarRashid67@users.noreply.github.com>
2023-02-06 01:03:46 +0600

    Time: 369 ms (70.79%), Space: 178 MB (13.07%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -3,18 +3,18 @@ public:
     int longestPalindrome(vector<string>& words) {
           map<pair<int,int> ,int>mp;
         for(auto x:words){
-             mp[{x[0]-'a',x[1]-'a'}]++;
+             mp[{x[0],x[1]}]++;
         }
         int ans=0;
-        for(int i=0;i<27;i++){
-            for(int j=0;j<27;j++){
+        for(int i=97;i<130;i++){
+            for(int j=97;j<130;j++){
                 if(i!=j){
                     ans+=min(mp[{i,j}],mp[{j,i}]);
                 }
             }
         }
         int mx=0;
-        for(int i=0;i<27;i++){
+        for(int i=97;i<130;i++){
             ans+=mp[{i,i}];
             if(mp[{i,i}]%2==1){
                 ans--;
