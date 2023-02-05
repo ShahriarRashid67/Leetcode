0f488c542e45bc613e2afeaeb28da7465c91f7fa
ShahriarRashid67 <102477800+ShahriarRashid67@users.noreply.github.com>
2023-02-06 01:08:13 +0600

    Time: 409 ms (57.87%), Space: 178 MB (13.07%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-         int count[26][26] = {}; 
-       int ans =0;
-        
-			for(auto w : words){
-				int a = w[0] - 'a';
-				int b = w[1] - 'a'; 
-
-				if(count[b][a]){
-					ans+= 4;          
-					count[b][a]--;   
-				}else
-					count[a][b]++;  
-			}
-        
-			for(int i=0;i<26;i++){
-				if(count[i][i]){
-					ans+=2;
-					break;
-				}
-			 }
-      
-	return ans;
+          map<pair<int,int> ,int>mp;
+        for(auto x:words){
+             mp[{x[0],x[1]}]++;
+        }
+        int ans=0;
+        for(int i=97;i<130;i++){
+            for(int j=97;j<130;j++){
+                if(i!=j){
+                    ans+=min(mp[{i,j}],mp[{j,i}]);
+                }
+            }
+        }
+        int mx=0;
+        for(int i=97;i<130;i++){
+            ans+=mp[{i,i}];
+            if(mp[{i,i}]%2==1){
+                ans--;
+                mx=1;
+            }
+           
+        }
+        return 2*(ans+mx);
     }
 };
